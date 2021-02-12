fdb4cdfc9e61213f920a9d39b03ac009bd3883a8
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-12 14:36:22 +0300

    chalk

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const chalk = require('chalk')
+// const chalk = require('chalk')
 
 const connect = runServer => {
   mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
@@ -13,7 +13,7 @@ const connect = runServer => {
   db.on('error', console.error.bind(console, 'connection error'))
   db.once('open', () => {
     // we are connected
-    console.log(chalk.blue('we are connected'))
+    // console.log(chalk.blue('we are connected'))
     runServer()
   })
 }
